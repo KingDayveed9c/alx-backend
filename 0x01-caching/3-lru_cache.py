d25ceffd172ad7c6c3c6a52df39e1d08b437add3
David Adejumo <71224787+KingDayveed9c@users.noreply.github.com>
2023-05-18 00:32:55 +0100

    Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,45 +1,39 @@
 #!/usr/bin/env python3
 """This module creates a class LRUCache that inherits from BaseCaching and is a caching system.
 """
-BaseCaching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
+
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """_summary_
+    """This is an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal principle when the limit is reached.
     """
-
     def __init__(self):
-        """_summary_
+        """This initializes the cache.
         """
         super().__init__()
-        self.usedKeys = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """_summary_
-
-        Args:
-                        key (_type_): _description_
-                        item (_type_): _description_
+        """This adds an item in the cache.
         """
-        if key is not None and item is not None:
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
             self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
-            else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(0)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """return the value in self.cache_data linked to key
-
-        Args:
-                        key (_type_): _description_
+        """Retrieves an item by key.
         """
-        if key is not None and key in self.cache_data.keys():
-            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
-            return self.cache_data.get(key)
-        return None
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
